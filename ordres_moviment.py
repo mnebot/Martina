4960a8b4e5a1e63fa6fdf3957ab7a0b9aabaf9af
Marçal Nebot <mnebot@gmail.com>
2018-03-08 18:51:08 +0100

    canvis a la capçalera

--- ordres_moviment.py
+++ ordres_moviment.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python3
-# Copyright 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """
+Licensed under the MIT License
 
-Google CloudSpeech recognizer utilitzat per a donar ordres a 
+Utilitza el projecte AIY de Google	 per a donar ordres a 
 la Martina en català.
+
+Utilitza el sintetitzador de veu Festival i la veu Ona de Fescat.
+
+Es comunica amb el robot mitjançant les llibreries de robotical.io martypy
  
 """
 
